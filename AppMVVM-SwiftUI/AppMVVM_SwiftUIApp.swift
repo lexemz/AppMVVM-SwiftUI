5910a0354bce189022a76bd5d7a8b849eb088480
Igor Buzykin <lexemz@gmail.com>
2022-03-04 15:30:15 +0300

    Initial Commit

---
+++ AppMVVM-SwiftUI/AppMVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMVVM_SwiftUIApp.swift
+//  AppMVVM-SwiftUI
+//
+//  Created by Igor on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppMVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
